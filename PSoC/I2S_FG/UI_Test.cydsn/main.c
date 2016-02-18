fbee71011de71da5d7be560a04a77a6994a8588f
ryood <kawanabe@gmail.com>
2016-02-19 08:53:25 +0900

    UI_Test: 電卓のようにキーパッドから周波数値を入力

--- PSoC/I2S_FG/UI_Test.cydsn/main.c
+++ PSoC/I2S_FG/UI_Test.cydsn/main.c
@@ -11,6 +11,8 @@
  *  RX: P12[6]
  *  TX: P13[7]
  *
+ * 2016.02.19 KeyPadで周波数の入力
+ * 2016.02.19 LCD SW
  * 2016.02.15 Rotary Encoder
  * 2016.02.15 KeyPad
  * 2016.02.14 I2C LCD Test
@@ -24,9 +26,32 @@
 #include "SPLC792-I2C.h"
 
 #define TITLE_STR1  ("I2S FG  ")
-#define TITLE_STR2  ("20160214")
+#define TITLE_STR2  ("20160219")
 
-int keyPadScan()
+#define CMD_CLR     12
+#define CMD_ENT     13
+#define CMD_MENU    14
+
+#define KEY_BUFFER_LENGTH   8
+
+const int POW10[] = {
+    1, 10, 100, 1000, 10000, 100000, 1000000, 10000000    
+};
+
+char uartBuff[80];
+
+int keyBuffer[KEY_BUFFER_LENGTH];
+int kbp;
+
+int frequency;
+int isLCDOn;
+
+//------------------------------------------------------
+// keyPadScan1(): 押されているキー
+// return: キースキャンの結果 0..15
+//         押されていない場合は -1
+//------------------------------------------------------
+int keyPadScan1()
 {
     uint8 i, j, r;
     for (i = 0; i < 4; i++) {
@@ -37,6 +62,65 @@ int keyPadScan()
                 return (int)i * 4 + j;
             }
         }
+    }
+    return -1;
+}
+
+//------------------------------------------------------
+// keyPadScan():
+// return: -1: 変化なし
+//         0 ..  9: 数字キー入力 結果はKeyBuffer1
+//         10:      A (LCD On)
+//         11:      B (LCD Off) 
+//         12:      C (クリア)
+//         13:      D (エンター)
+//         14:      * (メニュー)
+//         15:      # (未設定)
+//
+const int keyPadMatrix[] = {
+     1,  2,  3, 10,
+     4,  5,  6, 11,
+     7,  8,  9, 12,
+    14,  0, 15, 13 
+};
+
+int keyPadScan()
+{
+    static int prevKey = -1;
+    int krd, kv;
+
+    krd = keyPadScan1();
+    if (krd != prevKey) {
+        prevKey = krd;
+        kv = -1;
+        if (krd != -1) {
+            kv = keyPadMatrix[krd];
+            switch (kv) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                if (kbp < KEY_BUFFER_LENGTH) {
+                    keyBuffer[kbp] = kv;
+                    kbp++;
+                }
+                break;
+            case CMD_CLR:
+                kbp = 0;
+                break;
+            case CMD_MENU:
+                break;
+            case CMD_ENT:
+                break;
+            }
+        }
+        return kv;
     }
     return -1;
 }
@@ -57,21 +141,6 @@ int readRE()
     index = (index << 2) | rd;
 	index &= 0b1111;
 
-    /*
-	switch (index) {
-	// 時計回り
-	case 0b0001:	// 00 -> 01
-	case 0b1110:	// 11 -> 10
-	    retval = 1;
-	    break;
-	// 反時計回り
-	case 0b0010:	// 00 -> 10
-	case 0b1101:	// 11 -> 01
-	    retval = -1;
-	    break;
-    }
-    */
-    
     // Alps EC12E24208
     switch (index) {
     case 0b1101:
@@ -84,54 +153,85 @@ int readRE()
     return retval;
 }
 
+//-------------------------------------------------
+// LCDのOn/Off
+// parameter: sw: !0:on 0:off
+//
+void switchLCD(int swOn)
+{
+    if (0 != swOn) {
+        Pin_LCD_SW_Write(1u);
+        
+        Pin_I2C_RST_Write(0u);
+        CyDelay(1);
+        Pin_I2C_RST_Write(1u);
+        
+        // akizuki AQM0802(3.3V)
+        LCD_Init(0x3e, 32);        
+        CyDelay(1);
+    } else {
+        Pin_LCD_SW_Write(0u);
+        Pin_I2C_RST_Write(0u);
+    }
+}
+
 int main()
 {
     char buff1[80];
     char buff2[80];
-    char buff3[80];
-    int cnt, v;
+    int i, v, key;
     
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     UART_Start();
     UART_PutString("Start..\r\n");
     
-    
-    Pin_I2C_RST_Write(0u);
-    CyDelay(1);
-    Pin_I2C_RST_Write(1u);
-    
     I2CM_LCD_Start();
-    // akizuki AQM0802(3.3V)
-    LCD_Init(0x3e, 32);
     
-    CyDelay(100);
-
+    switchLCD(1);
+    isLCDOn = 1;
+    
     LCD_Puts(TITLE_STR1);
     LCD_SetPos(0, 1);
     LCD_Puts(TITLE_STR2);
     
     CyDelay(1000);
     
-    cnt = 0;
-    v = 0;
     for(;;)
     {
-        //Pin_LED_Write(Pin_LED_Read() ? 0 : 1);
+        key = keyPadScan();
+        if ((0 <= key && key <= 9) || (key == CMD_CLR) || (key == CMD_ENT)) {
+            v = 0;
+            for (i = 0; i < kbp; i++) {
+                v += keyBuffer[i] * POW10[kbp - 1 - i];
+            }
 
-        cnt++;
-        v += readRE();
-
-        sprintf(buff1, "%08d", cnt);
-        sprintf(buff2, "%d   ", keyPadScan());
-        sprintf(buff3, "%d   ", v);
+            if (key == CMD_ENT) {
+                frequency = v;
+                kbp = 0;
+            }
+            sprintf(buff1, "%08d", v); 
+        }
+        if (key == CMD_MENU) {
+            sprintf(buff1, "MENU?   ");
+        }
+        
+        frequency += readRE();
+        sprintf(buff2, "%8d", frequency);
 
         LCD_SetPos(0, 0);
         LCD_Puts(buff1);
         LCD_SetPos(0, 1);
         LCD_Puts(buff2);
-        LCD_SetPos(4, 1);
-        LCD_Puts(buff3);
+
+        if (!isLCDOn && key == 10) {
+            switchLCD(1);
+            isLCDOn = 1;
+        }
+        if (isLCDOn && key == 11){
+            switchLCD(0);
+            isLCDOn = 0;
+        }
         
         CyDelay(1);        
     }
